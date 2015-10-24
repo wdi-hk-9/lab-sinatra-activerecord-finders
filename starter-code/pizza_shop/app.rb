dddd5217e52de1783e76983245729a7df43b778c
Philippa Johnstone <pecj@hotmail.co.uk>
2015-10-24 20:16:15 +0800

    RESTful controller with ActiveRecord methods updated in app.rb

--- starter-code/pizza_shop/app.rb
+++ starter-code/pizza_shop/app.rb
@@ -1,2 +1,56 @@
 class PizzaShop < Sinatra::Base
+
+#root route
+get "/" do
+  redirect('/pizzas')
+end
+
+# index
+get '/pizzas' do
+  @pizza = Pizza.all
+  erb(:"pizzas/new")
+end
+
+#new
+get '/pizzas' do
+  @pizza = Pizza.new
+  erb(:"pizzas/new")
+end
+
+#create
+post '/pizzas' do
+  @psizza = Pizza.new(params[:song])
+  if @ pizza.save
+    redirect("/pizzas/#{@pizza.id}")
+  else
+    erb(:"pizzas/new")
+end
+
+# show
+get '/pizzas/:id' do
+  @pizza = Pizza.find(params[:id])
+  erb(:"pizzas/show")
+end
+
+#update
+post '/pizzas/:id/update' do
+  @pizza = Pizza.find(params[:id])
+  @pizza.update_attributes(params[:pizza])
+    if @pizza.save
+    redirect("/pizzas")
+  else
+    erb(:"pizzas/new")
+  end
+end
+
+# destroy
+post '/pizzas/:id/delete' do
+  @pizza = Pizza.find(params[:id])
+  if @pizza.destroy
+    redirect('/pizzas')
+  else
+    redirect("/pizzas/#{@pizza.id}")
+  end
+end
+
 end
--- starter-code/pizza_shop/config.ru
+++ starter-code/pizza_shop/config.ru
@@ -2,7 +2,10 @@ require 'rubygems'
 require 'bundler'
 Bundler.require
 
+#models
 require './models/pizza'
+
+#controllers
 require './app'
 
 run PizzaShop
